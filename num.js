f62becf6066e7223a5d49234a7155779dffe2f5b
alexacost <ala@hazear.com>
2021-07-11 22:51:04 +0000

    Cambios JS

--- num.js
+++ num.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
     $("button").click(function(){
         var name = ($("#nombre").val());
+        var nameLower = name.toLowerCase();
         var count = name.length;
+        var nameSplit = nameLower.split('');
+        var suma = 0;
+        for(i=0;i<count;i++) {
+            if (nameSplit[i]==='a' || nameSplit[i]==='j') {
+                suma = suma + 1;
+            }
+        }
+        console.log(suma)
+
     });
   });
